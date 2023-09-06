db0008f4ab6d6abe35dc6dd405f3504f05e6fdf8
mkor92 <mkorhonen92@gmail.com>
2023-09-06 10:12:32 +0200

    added while loop to menu

--- src/main/java/org/example/Menu.java
+++ src/main/java/org/example/Menu.java
@@ -7,56 +7,64 @@ public class Menu {
     public static void menu() {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Elpriser");
-        System.out.println("========");
-        System.out.println("1. Inmatning");
-        System.out.println("2. Min, Max och Medel");
-        System.out.println("3. Sortera");
-        System.out.println("4. Bästa Laddningstid (4h)");
-        System.out.println("e. Avsluta");
-        String menuInput = scanner.nextLine();
+        boolean continueMenu = true;
+        while (continueMenu) {
+            System.out.println("Elpriser");
+            System.out.println("========");
+            System.out.println("1. Inmatning");
+            System.out.println("2. Min, Max och Medel");
+            System.out.println("3. Sortera");
+            System.out.println("4. Bästa Laddningstid (4h)");
+            System.out.println("e. Avsluta");
+            String menuInput = scanner.nextLine();
 
-        switch (menuInput) {
-            case "e", "E" -> {
-                System.out.println("Programmet avslutades");
-                System.exit(0);
-            }
-            case "1" -> PriceInput.priceInput();
-            case "2" -> {
-                if (PriceInput.isPriceInput) {
-                    MinMax.minMax();
-                } else {
-                    getMsg();
-                    menu();
+            switch (menuInput) {
+                case "e", "E" -> {
+                    System.out.println("Programmet avslutades");
+                    System.exit(0);
                 }
-            }
-            case "3" -> {
-                if (PriceInput.isPriceInput) {
-                    SortPrices.sortPrices();
-                } else {
-                    getMsg();
-                    menu();
+                case "1" -> {
+                    if (!PriceInput.isPriceInput) {
+                        PriceInput.priceInput();
+                    } else {
+                        System.out.println("\nDu har redan matat in dagens priser!\n");
+                    }
                 }
-            }
-            case "4" -> {
-                if (PriceInput.isPriceInput) {
-                    ChargeTime.chargeTime();
-                } else {
-                    getMsg();
-                    menu();
+                case "2" -> {
+                    if (PriceInput.isPriceInput) {
+                        MinMax.minMax();
+                    } else {
+                        getMsg();
+
+                    }
                 }
-            }
-            default -> {
-                System.out.println(menuInput + " är inte ett giltigt val!");
-                menu();
+                case "3" -> {
+                    if (PriceInput.isPriceInput) {
+                        SortPrices.sortPrices();
+                    } else {
+                        getMsg();
+
+                    }
+                }
+                case "4" -> {
+                    if (PriceInput.isPriceInput) {
+                        ChargeTime.chargeTime();
+                    } else {
+                        getMsg();
+
+                    }
+                }
+                default -> System.out.println(menuInput + " är inte ett giltigt val!");
+
+
             }
         }
     }
 
     public static void getMsg() {
-        System.out.println("Vänligen börja med att mata in dagens priser\n");
+        System.out.println("\nVänligen börja med att mata in dagens priser\n");
     }
 
-    
+
 }
 
